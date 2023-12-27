a76526da3e1b2d3b92435aea8f388281f31a165b
aipengLi <2333850040c@hiro.kindai.ac.jp>
2023-12-27 13:32:33 +0900

    The functionalities for deleting and updating categories have been updated, and they have been encapsulated into functions.

--- admin/categories.php
+++ admin/categories.php
@@ -1,4 +1,5 @@
 <?php include "includes/admin_header.php"; ?>
+<?php include "functions.php"; ?>
 
 <div id="wrapper">
 
@@ -19,23 +20,7 @@
 
                     <div class="col-xs-6">
 
-                        <?php
-                        if (isset($_POST['submit'])) {
-                            $cat_title = $_POST['cat_title'];
-                            if ($cat_title == "" || empty($cat_title)) {
-                                echo "This field should not be empty";
-                            } else {
-                                $query = "INSERT INTO categories(cat_title) ";
-                                $query .= "VALUE('{$cat_title}') ";
-
-                                $create_category_query = mysqli_query($connection, $query);
-
-                                if (!$create_category_query) {
-                                    die('QUERY FAILED' . mysqli_error($connection));
-                                }
-                            }
-                        }
-                        ?>
+                        <?php insert_categories(); ?>
 
 
                         <form action="" method="post">
@@ -47,15 +32,18 @@
                                 <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                             </div>
                         </form>
+
+                        <?php
+                        //UPDATE AND INCLUDE QUERY
+                        if (isset($_GET['edit'])) {
+                            $cat_id = $_GET['edit'];
+
+                            include "includes/update_categories.php";
+                        }
+                        ?>
                     </div>
 
                     <div class="col-xs-6">
-
-                        <?php
-                        $query = 'SELECT * FROM categories';
-                        $select_categories_sidebar = mysqli_query($connection, $query);
-                        ?>
-
                         <table class="table table-bordered table-hover">
                             <thead>
                                 <tr>
@@ -66,14 +54,13 @@
                             <tbody>
 
                                 <?php
-                                while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
-                                    $cat_id = $row['cat_id'];
-                                    $cat_title = $row['cat_title'];
-                                    echo "<tr>";
-                                    echo "<td>{$cat_id}</td>";
-                                    echo "<td>{$cat_title}</td>";
-                                    echo "</tr>";
-                                }
+                                //FIND ALL CATEGORIES QUERY
+                                findAllCategories();
+                                ?>
+
+                                <?php
+                                //DELETE QUERY
+                                deleteCategories();
                                 ?>
 
                             </tbody>
